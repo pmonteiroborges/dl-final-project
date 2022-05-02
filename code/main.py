076846192ceb42ba1d0a2c37e8bd280412c848ef
arrie <arrie@arringtons-mbp.devices.brown.edu>
2022-05-02 17:30:57 -0400

    add stencil to main.py

---
+++ code/main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import tensorflow as tf
+
+from model import Model
+from preprocess import get_data, pad_corpus, convert_to_id
+
+def train(model, train_input, train_output, padding_index):
+    pass
+
+
+def test(model, test_input, test_output, padding_index):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
